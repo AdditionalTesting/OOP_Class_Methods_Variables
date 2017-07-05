5dab90430119ad1ac5a1159b74236060302c20c0
AlB <albert.burc@gmail.com>
2017-07-05 18:40:32 -0400

    finished skeleton for zombies exercise

---
+++ part3.rb
@@ -0,0 +1,50 @@
+class Zombie
+
+  @@horde = []
+  @@plague_level = 10
+  @@max_speed = 5
+  @@max_strength = 8
+  @@default_speed = 1
+  @@default_strength = 3
+
+  def initialize
+
+  end
+
+  def encounter
+
+  end
+
+  def outrun_zombie?
+
+  end
+
+  def survive_attack?
+
+  end
+
+  def self.all
+
+  end
+
+  def self.new_day
+
+  end
+
+  def self.some_die_off
+
+  end
+
+  def self.spawn
+
+  end
+
+  def self.increase_plague_level
+
+  end
+
+
+
+
+
+end
