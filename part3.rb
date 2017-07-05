97ca793db8bbf18e130cb65bd18b950807ab206e
AlB <albert.burc@gmail.com>
2017-07-05 19:06:57 -0400

    getting through the zombies

--- part3.rb
+++ part3.rb
@@ -7,7 +7,17 @@ class Zombie
   @@default_speed = 1
   @@default_strength = 3
 
-  def initialize
+  def initialize(speed, strength)
+    if speed > @@max_speed
+      @speed = @@default_speed
+    else
+      @speed = speed
+    end
+    if strength > @@max_strength
+      @strength = @@default_strength
+    else
+      @strength = strength
+    end
 
   end
 
@@ -24,23 +34,28 @@ class Zombie
   end
 
   def self.all
-
+    @@horde
   end
 
   def self.new_day
-
   end
 
   def self.some_die_off
-
+    die = rand(11).times do @@horde.pop end
   end
 
   def self.spawn
-
+    num = rand(@@plague_level)
+    num.times do
+      zombie = Zombie.new(rand(@@max_speed + 1), rand(@@max_strength + 1))
+      @@horde << zombie
+    end
+    @@horde
   end
 
   def self.increase_plague_level
-
+    @@plague_level = rand(3) + @@plague_level
+    return @@plague_level
   end
 
 
