b725db5eb0a87b04ac4b90ab07e1ee659fca0abc
AlB <albert.burc@gmail.com>
2017-07-05 19:49:38 -0400

    almost killed off the zombies

--- part3.rb
+++ part3.rb
@@ -7,6 +7,8 @@ class Zombie
   @@default_speed = 1
   @@default_strength = 3
 
+  attr_accessor :speed, :strength
+
   def initialize(speed, strength)
     if speed > @@max_speed
       @speed = @@default_speed
@@ -22,14 +24,28 @@ class Zombie
   end
 
   def encounter
+    random = rand(2)
+    if random == 0
 
   end
 
   def outrun_zombie?
+    how_fast = rand(0..@@max_speed)
+    if how_fast > self.speed
+      return true
+    else
+      return false
+    end
 
   end
 
   def survive_attack?
+    how_strong = rand(0..@@max_strength)
+    if how_strong > self.strength
+      return true
+    else
+      return false
+    end
 
   end
 
@@ -38,10 +54,14 @@ class Zombie
   end
 
   def self.new_day
+    self.some_die_off
+    self.spawn
+    self.increase_plague_level
+
   end
 
   def self.some_die_off
-    die = rand(11).times do @@horde.pop end
+    rand(11).times do @@horde.pop end
   end
 
   def self.spawn
